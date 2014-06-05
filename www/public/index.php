a1d2d76f397e6752e9f3313e7afc7de0e1559a8f
root <root@ip-10-252-30-231.us-west-2.compute.internal>
2014-06-05 05:01:08 +0000

    commit

---
+++ www/public/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Freediving.Ninja</title>
+    <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <style>
+html{
+height:100%;
+}
+	body{
+height:100%;
+background: #4f7ebc; /* Old browsers */
+background: -moz-linear-gradient(top, #4f7ebc 0%, #2989d8 32%, #207cca 56%, #1e5799 100%); /* FF3.6+ */
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#4f7ebc), color-stop(32%,#2989d8), color-stop(56%,#207cca), color-stop(100%,#1e5799)); /* Chrome,Safari4+ */
+background: -webkit-linear-gradient(top, #4f7ebc 0%,#2989d8 32%,#207cca 56%,#1e5799 100%); /* Chrome10+,Safari5.1+ */
+background: -o-linear-gradient(top, #4f7ebc 0%,#2989d8 32%,#207cca 56%,#1e5799 100%); /* Opera 11.10+ */
+background: -ms-linear-gradient(top, #4f7ebc 0%,#2989d8 32%,#207cca 56%,#1e5799 100%); /* IE10+ */
+background: linear-gradient(to bottom, #4f7ebc 0%,#2989d8 32%,#207cca 56%,#1e5799 100%); /* W3C */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#4f7ebc', endColorstr='#1e5799',GradientType=0 ); /* IE6-9 */
+}
+    </style>
+</head>
+<body>
+	<img src="/img/image.png" style="display:block; margin:3em auto;" alt="Freediving Ninja" />
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-10817444-26', 'freediving.ninja');
+  ga('require', 'displayfeatures');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
